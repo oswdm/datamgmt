100b9c580375e10c1007d7942ff59ecabc6e3486
thueradr <thueradr@students.zhaw.ch>
2024-05-12 23:29:35 +0200

    created qdrant db connector

--- dbs/__init__.py
+++ dbs/__init__.py
@@ -2,8 +2,9 @@ from enum import Enum
 
 from .chromadb import Chroma
 from .pgvector import PGVector
-
+from .qdrant import Qdrant
 
 class DBS(Enum):
     chromadb = Chroma
     pgvector = PGVector
+    qdrant = Qdrant
