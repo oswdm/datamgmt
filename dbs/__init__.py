0fd2f91708ee3302c1b22e3f970242a36a2bdb9d
Felix Saaro <saaf@zhaw.ch>
2024-05-13 15:42:56 +0200

    Add vespa implementation

--- dbs/__init__.py
+++ dbs/__init__.py
@@ -2,8 +2,9 @@ from enum import Enum
 
 from .chromadb import Chroma
 from .pgvector import PGVector
-
+from .vespadb import VespaDB
 
 class DBS(Enum):
     chromadb = Chroma
     pgvector = PGVector
+    vespa = VespaDB
