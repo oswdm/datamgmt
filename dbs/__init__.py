3823c3ac105e684270f247dcc88ffeeb4837ea3c
Felix Saaro <saaf@zhaw.ch>
2024-05-13 15:29:16 +0200

    dvc: commit experiment 270c1bc7ca00423e788d2631a5b31eb41c0adb73fdf36c0a9064ae766d9ff3e0

--- dbs/__init__.py
+++ dbs/__init__.py
@@ -2,8 +2,10 @@ from enum import Enum
 
 from .chromadb import Chroma
 from .pgvector import PGVector
+from .vespadb import VespaDB
 
 
 class DBS(Enum):
-    chromadb = Chroma
-    pgvector = PGVector
+    # chromadb = Chroma
+    # pgvector = PGVector
+    vespa = VespaDB
