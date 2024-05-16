8ffc9f389a6a94489e9481bdad2ad2f5bc3f19d5
R4ngered <61281321+R4ngered@users.noreply.github.com>
2024-05-16 15:26:05 +0200

    Milvus (#5)
    * added dbconnector milvus
    
    * removed volumes as no persistent data needed

--- dbs/__init__.py
+++ dbs/__init__.py
@@ -3,10 +3,12 @@ from enum import Enum
 from .chromadb import Chroma
 from .pgvector import PGVector
 from .vespadb import VespaDB
+from .milvus import Milvus
 from .qdrant import Qdrant
 
 class DBS(Enum):
     chromadb = Chroma
     pgvector = PGVector
     vespa = VespaDB
+    milvus = Milvus
     qdrant = Qdrant
